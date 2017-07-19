310bdb884a69b6446a5a706d181049481897b416
Unknown <riza.ega.s@mail.ugm.ac.id>
2017-07-19 21:25:07 +0700

    -

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'react-redux';
 
 class PostsNew extends Component {
   render() {
     return (
-      <div>
-        Post New!
-      </div>
+     <form>
+     {/* Field is used to represent a distinct input that will be visible on the screen*/}
+      <Field
+        name="title"
+        component={}
+      />
+     </form> 
     );
   }
 }
 
-export default PostsNew;
+// This is simlar to connect function in redux
+// This is to connect our form with our reducer
+export default reduxForm({
+  // Must be unique
+  form: 'PostsNewForm'
+})(PostsNew);
